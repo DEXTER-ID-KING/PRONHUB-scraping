496d6dabf0e97067816f77ef31ddc1ebac7a31cf
DEXTER-ID-KING <dexternew785@gmail.com>
2025-05-11 03:04:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+
 const express = require('express');
 const app = express();
+const pornhub = require('./api/pornhub');
+const xvideos = require('./api/xvideos');
+const youporn = require('./api/youporn');
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Porn Scraper API Running');
 });
 
-module.exports = app;
+app.get('/api/pornhub', pornhub);
+app.get('/api/xvideos', xvideos);
+app.get('/api/youporn', youporn);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
