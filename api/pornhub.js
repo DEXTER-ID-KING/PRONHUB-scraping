c3b73a199476c9650e8c28c6ada894397b09407a
DEXTER-ID-KING <dexternew785@gmail.com>
2025-05-11 02:33:03 +0530

    Create pornhub.js

---
+++ api/pornhub.js
@@ -0,0 +1,21 @@
+const { getBrowser } = require('../utils/puppeteerClient');
+
+module.exports = async (req, res) => {
+  const { category = '29' } = req.query;
+  const url = `https://www.pornhub.com/video?c=${category}`;
+
+  const browser = await getBrowser();
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: 'networkidle2' });
+
+  const videos = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('.videoPreviewBg')).map(v => ({
+      title: v.querySelector('img')?.alt,
+      thumbnail: v.querySelector('img')?.src,
+      url: 'https://www.pornhub.com' + v.closest('a')?.getAttribute('href'),
+    }));
+  });
+
+  await browser.close();
+  res.status(200).json({ videos });
+};
