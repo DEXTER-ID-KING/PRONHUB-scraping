08d0d7cff3124a20c5b7224ff7eba81952b75df7
DEXTER-ID-KING <dexternew785@gmail.com>
2025-05-11 02:29:00 +0530

    Create youporn.js

---
+++ api/youporn.js
@@ -0,0 +1,20 @@
+const { getBrowser } = require('../utils/puppeteerClient');
+
+module.exports = async (req, res) => {
+  const url = 'https://www.youporn.com';
+
+  const browser = await getBrowser();
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: 'networkidle2' });
+
+  const videos = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('.video-box')).map(v => ({
+      title: v.querySelector('.video-title')?.textContent.trim(),
+      thumbnail: v.querySelector('img')?.getAttribute('data-thumbnail') || v.querySelector('img')?.src,
+      url: 'https://www.youporn.com' + v.querySelector('a')?.getAttribute('href'),
+    }));
+  });
+
+  await browser.close();
+  res.status(200).json({ videos });
+};
